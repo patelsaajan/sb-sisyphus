0271d29f9aa9549a008d8ae0835ea3567b46c63a
Saajan Patel <saajan.patel@hoppstudio.com>
2025-02-09 17:34:39 +0000

    feat: recipe modal design

--- app/components/modals/recipe.vue
+++ app/components/modals/recipe.vue
@@ -1,15 +1,61 @@
 <template>
     <UModal>
-        <UCard>
-            <span>this is a test header</span>
-            <span>this is a test body</span>
-            <span>this is a test footer</span>
+        <UCard
+            class=""
+                :ui = "{ 
+                        divide: 'divide-none',
+                        }"
+        >
+            <template #header>
+                <div class="flex flex-col font-bold gap-y-4 text-center">
+                    {{ recipe.name }}
+                    <div class="flex justify-center">
+                        <NuxtPicture 
+                            format="jpg"
+                            src="/imgs/default-food.jpg"
+                            alt="Recipe of the day"
+                            width="250px"
+                            height="250px"
+                              :imgAttrs="{'class': 'rounded-lg'}"
+                        />
+                    </div> 
+                </div>
+            </template>
+            <div class="flex flex-col gap-4 text-grey">
+                <div class="flex flex-row gap-6">
+                    <span class="text-secondary"> {{ recipe.cuisine }} </span>
+                    <span class="flex flex-row items-center gap-x-2"> <Icon name="material-symbols:timer-outline-rounded" size="1.5rem"/>  {{ recipe.preparationTime }} minutes</span>
+                    <span class="flex flex-row items-center gap-x-2 "><Icon name="ic:round-people" size="1.5rem"/>  {{ recipe.servings }} servings</span>
+                </div>
+                <UButton color="primary" class="max-w-fit" @click="visitRecipeClick">
+                    View Full Recipe
+                </UButton>
+                <span class="">{{ recipe.description }}</span>
+                <span class="font-semibold">Ingredients</span>
+                <span class="font-semibold">Instructions</span>
+            </div>
+            <template #footer>
+                <UButton color="primary" class="max-w-fit" @click="visitRecipeClick">
+                    View Full Recipe
+                </UButton>
+            </template>
         </UCard>
-
     </UModal>    
 </template>
 
-<script setup>
+<script setup lang="ts">
+import type IRecipes from '~~/types/recipes';
+
+const props = defineProps({
+    recipe: {
+        type: Object as () => IRecipes,
+        required: true
+    }
+})
+
+const visitRecipeClick = () => {
+    console.log('Visit Recipe Clicked');
+}
 
 </script>
 
